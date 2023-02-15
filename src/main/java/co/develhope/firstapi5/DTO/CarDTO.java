af07684575edc8e78ecc8573854be866dc1734e5
Daniele Caramanica <danielecaramanica@gmail.com>
2023-02-15 14:48:32 +0100

    Update ex

--- src/main/java/co/develhope/firstapi5/DTO/CarDTO.java
+++ src/main/java/co/develhope/firstapi5/DTO/CarDTO.java
@@ -22,4 +22,36 @@ public CarDTO(String id, String modelName, double price) {
         this.price = price;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "CarDTO{" +
+                "id='" + id + '\'' +
+                ", modelName='" + modelName + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
